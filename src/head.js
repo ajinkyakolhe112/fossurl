b9c212b573f9268108ab442fef3d85e1b87e8674
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2020-04-26 20:02:34 +0530

    Update head.js

--- src/head.js
+++ src/head.js
@@ -1,4 +1,4 @@
-var endpoint = "https://jsonbox.io/box_f4a3d05ef8fe374ac8ea";
+var endpoint = "https://www.jsonstore.io/984265cf278a2655f7f3d3c740ff9227152aff9d4d2d47f0359ff2c89357f9d0";
 
 function fetchJSON(a) {
     var f = new XMLHttpRequest;
